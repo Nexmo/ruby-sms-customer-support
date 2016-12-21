4f74f4cb61ae8aaa9de0e2a123d587b45caa7bdb
Cristiano Betta <cbetta@gmail.com>
2016-12-21 16:02:04 +0000

    Changed order of some of the commands

--- commands.sh
+++ commands.sh
@@ -1,11 +1,12 @@
 rails new customer-support
+cd customer-support
 rake db:create db:migrate
+rails g controller pages index
 rails s
-nexmo number:ssearch GB --sms
+nexmo number:search GB --sms
 nexmo number:buy [your_number] --confirm
 nexmo link:sms [your_number] http://your.domain/support --help
-rails g controller pages index
-rails g model Ticket number
-rails g model Message text ticket:references 
-rake db:migrate
 rails g controller support index
+rails g model Ticket number
+rails g model Message text ticket:references
+rake db:migrate
